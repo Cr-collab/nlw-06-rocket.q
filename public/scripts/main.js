7277d9517660a6808b3d275ef2c87aba61d9b311
Cristiano Azevdo <cristiccorrea@gmail.com>
2021-06-26 12:46:11 -0300

    Ulimo stage finalizado

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,55 +1,44 @@
-import Modal from './modal.js';
+  
+import Modal from './modal.js'
 
-const modal = Modal();
+const modal = Modal()
 
-//pegar todos botãoes que existe com a classe check
-const checkButtons = document.querySelectorAll('.actions a.check');
-
-const modalTitle = document.querySelector('.modal h2');
-const modalDescription = document.querySelector('.modal p');
-const modalButton = document.querySelector('.modal button');
+const modalTitle = document.querySelector('.modal h2')
+const modalDescription = document.querySelector('.modal p')
+const modalButton = document.querySelector('.modal button')
 
 
-checkButtons.forEach(button => {button.addEventListener('click', (event)=>{  handleClick( event , true)} )});
-// for( const button of checkButtons) {
+//Pegar todos os botões que existe com a classe check
+const checkButtons = document.querySelectorAll(".actions a.check") 
 
-//     button.addEventListener('click', (event)=>{
-//        //abrir modal
-//         modal.open();
-//     })
-
-// }
+checkButtons.forEach(button => {
+    //adicionar a escuta
+    button.addEventListener("click", handleClick)
+})
 
 
-/** Quando o delete for clickado ele abre a modal */
-const deleteButtons = document.querySelectorAll('.actions a.delete');
+/*Quando o botão delete for clicado ele abre a modal */
+const deleteButton = document.querySelectorAll(".actions a.delete")
 
-deleteButtons.forEach(button => {button.addEventListener('click', (event)=>{  handleClick( event , false) } )});
+deleteButton.forEach(button => {
+    button.addEventListener("click", (event) => handleClick(event, false))
+})
 
+function handleClick(event, check = true){
+    event.preventDefault()
+    const text = check ? "Marcar como lida" : "Excluir"
+    const slug = check ? "check" : "delete"
+    const roomId = document.querySelector("#room-id").dataset.id
+    const questionId = event.target.dataset.id
 
-function handleClick(event , check = true) 
-{
-    event.preventDefault();
-    const text = check  ? "Marcar como lida" : "Excluir";
-    const slug = check ? "check": "delete";
+    const form = document.querySelector(".modal form")
+    form.setAttribute("action", `/question/${roomId}/${questionId}/${slug}`)
 
-    const roomId = document.querySelector("#room-id").dataset.id;
-    const questionId =  event.target.dataset.id;
-
-    const form = document.querySelector(".modal form");
-    form.setAttribute("action", `/questions/${roomId}/${questionId}/${slug}`)
-
-    modalTitle.innerHTML = `${text} esta pergunta`
-    modalDescription.innerHTML =`Tem certeza que deseja ${text.toLowerCase()} esta pergunta`
-    modalButton.innerHTML = `Sim, ${text.toLowerCase()}`
-    if(check) {modalButton.classList.remove('red')} else {modalButton.classList.add('red')}
-
-
+    modalTitle.innerHTML= `${text} esta pergunta`
+    modalDescription.innerHTML= `Tem certeza que deseja ${text.toLowerCase()} esta pergunta?`
+    modalButton.innerHTML= `Sim, ${text.toLowerCase()}`
+    check ? modalButton.classList.remove("red") : modalButton.classList.add("red")
+    
     //abrir modal
-    modal.open();
+    modal.open()
 }
-
-
-
-
-
--- src/route.js
+++ src/route.js
@@ -7,11 +7,14 @@ const route = express.Router();
 
 route.get('/', (req,res)=> res.render('home' , {page:"enter-room"}));
 route.get('/create-pass', (req,res)=> res.render('home',  {page:"create-pass"}));
-route.get('/room/:room', (req,res)=> res.render('room'));
-//Formato que o formulario tem que passar de dentro da modal 
-route.post('/question/:room/:question/:action', QuestionController.index);
-route.post('/create-room', RoomController.create);
 
+
+route.post('/create-room', RoomController.create);
+route.get('/room/:room', RoomController.open);
+route.post('/enterroom', RoomController.enter);
+
+ route.post('/question/create/:room', QuestionController.create)
+route.post('/question/:room/:question/:action', QuestionController.index);
 
 
 module.exports = route;
